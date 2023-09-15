a2245d7da403c74ba1f79aa1d51be056a578959b
KyawNyeinNaing <thetpai.dev@gmail.com>
2023-09-15 17:09:30 +0700

    updated: Image component

--- src/page-containers/Home.tsx
+++ src/page-containers/Home.tsx
@@ -1,5 +1,5 @@
-'use client';
-import { People, PeopleResult } from '@/types';
+import { Card, CardContent } from '@/components/Card';
+import { PeopleResult } from '@/types';
 import React from 'react';
 
 interface Props {
@@ -7,7 +7,38 @@ interface Props {
 }
 
 const Home: React.FC<Props> = ({ people }: Props) => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Card>
+        <CardContent className="p-2">
+          <div className="flex items-center">
+            <p className="text-sm text-muted-foreground">
+              Username :
+            </p>
+            &nbsp;
+          </div>
+          <div className="flex items-center">
+            <p className="text-sm text-muted-foreground">
+              Bio :
+            </p>
+            &nbsp;
+          </div>
+          <div className="flex items-center">
+            <p className="text-sm text-muted-foreground">
+              Source ID :
+            </p>
+            &nbsp;
+          </div>
+          <div className="flex items-center">
+            <p className="text-sm text-muted-foreground">
+              ID :
+            </p>
+            &nbsp;
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Home;
